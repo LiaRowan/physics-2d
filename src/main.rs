7231d022dfe074f4c8d7b6d717e358d707d73bdf
LiaRowan <LiaRowan3@gmail.com>
2018-10-15 18:17:59 -0400

    Add level rendering and init player

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate amethyst;
 mod state;
 
 use amethyst::{
+    core::transform::TransformBundle,
     prelude::{ Application, Config, GameDataBuilder },
     renderer::{
         DisplayConfig,
@@ -18,13 +19,14 @@ use state::Init;
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
-    let display_config = DisplayConfig::load("./resources/display_config.ron");
+    let display_config = DisplayConfig::load("./config/display_config.ron");
     let pipe = Pipeline::build().with_stage(
         Stage::with_backbuffer()
             .clear_target([1., 1., 1., 1.], 1.)
             .with_pass(DrawFlat::<PosTex>::new()),
     );
     let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
         .with_bundle(RenderBundle::new(pipe, Some(display_config)))?;
 
     Application::new("./", Init, game_data)?
--- src/state/init.rs
+++ src/state/init.rs
@@ -1,6 +1,20 @@
 use amethyst::{
-    prelude::{ GameData, State, StateData, Trans },
-    renderer::Event,
+    assets::{ AssetStorage, Loader },
+    core::{
+        cgmath::{ Matrix4, Vector3 },
+        transform::{ GlobalTransform, Transform },
+    },
+    prelude::*,
+    renderer::{
+        Camera,
+        Event,
+        PngFormat,
+        Projection,
+        Sprite,
+        Texture,
+        TextureHandle,
+        WithSpriteRender,
+    },
 };
 
 
@@ -11,8 +25,65 @@ impl<'a, 'b> State<GameData<'a, 'b>> for Init {
         Trans::None
     }
 
-    fn update(&mut self, data: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
-        data.data.update(&data.world);
+    fn update(&mut self, state: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
+        state.data.update(&state.world);
         Trans::None
     }
+
+    fn on_start(&mut self, state: StateData<GameData>) {
+        let spritesheet = get_spritesheet(state.world);
+
+        initialize_camera(state.world);
+        initialize_player(state.world, spritesheet);
+    }
+}
+
+
+const ARENA_WIDTH: f32 = 160.;
+const ARENA_HEIGHT: f32 = 160.;
+
+const SPRITE_UNIT: f32 = 16.;
+const SPRITESHEET_SIZE: (f32, f32) = (SPRITE_UNIT * 4., SPRITE_UNIT * 2.);
+
+fn get_spritesheet(world: &World) -> TextureHandle {
+    let loader = world.read_resource::<Loader>();
+    let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+
+    loader.load(
+        "./assets/spritesheets/shapes.png",
+        PngFormat,
+        Default::default(),
+        (),
+        &texture_storage,
+    )
+}
+
+fn initialize_camera(world: &mut World) {
+    world.create_entity()
+        .with(Camera::from(Projection::orthographic(
+            0., ARENA_WIDTH, ARENA_HEIGHT, 0.,
+        )))
+        .with(GlobalTransform(
+            Matrix4::from_translation(Vector3::new(0., 0., 1.)).into()
+        ))
+        .build();
+}
+
+fn initialize_player(world: &mut World, spritesheet: TextureHandle) {
+    let sprite = Sprite{
+        left: 0.,
+        right: SPRITE_UNIT,
+        top: 0.,
+        bottom: SPRITE_UNIT,
+    };
+    let mut transform = Transform::default();
+    transform.translation[0] = ARENA_WIDTH / 2.;
+    transform.translation[1] = ARENA_HEIGHT / 2.;
+
+    world.create_entity()
+        .with_sprite(&sprite, spritesheet, SPRITESHEET_SIZE)
+        .expect("Failed to add sprite render on player")
+        .with(GlobalTransform::default())
+        .with(transform)
+        .build();
 }
